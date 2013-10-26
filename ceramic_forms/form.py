a0e11d6e240957be1060fe1b0b281a68e06af695
Philipp Volugine <phil.volguine@gmail.com>
2013-10-26 01:18:16 -0400

    validate keys is its own function. all tests (so far) pass)

--- ceramic_forms/form.py
+++ ceramic_forms/form.py
@@ -9,8 +9,9 @@ class XOr:
     pass
 
 class If:
-    def __init__(self, *args):
-        pass
+    def __init__(self, paths, key):
+        self.paths = paths
+        self.key = key
 
 class FormErr(dict):
     def __init__(self, *args, **kwargs):
@@ -23,6 +24,43 @@ class FormErr(dict):
             value = []
             self[key] = value
         return value
+
+def path_exists(path, structure):
+    pass
+
+def validate_key(key, suspicious, reference_value, cleaned, errors):
+    if isinstance(key, Optional):
+        key = key.key
+        if key in suspicious:
+            validate_key(key, suspicious, reference_value, cleaned, errors)
+    elif key == Or:
+        validated = False
+        for orkey, orvalue in reference_value.items():
+            if orkey in suspicious:
+                validate_key(orkey, suspicious, orvalue, cleaned, errors)
+                validated = True
+        if not validated:
+            errors.section_errors.append(
+                "Missing any of {}".format(reference_value.keys()))
+    elif key == XOr:
+        validated = 0
+        for orkey, orvalue in reference_value.items():
+            if orkey in suspicious:
+                validate_key(orkey, suspicious, orvalue, cleaned, errors)
+                validated += 1
+        if validated == 0:
+            errors.section_errors.append(
+                "Missing one of {}".format(reference_value.keys()))
+        elif validated > 1:
+            errors.section_errors.append(
+                "Only one of {} permitted".format(reference_value.keys()))
+    elif isinstance(key, If):
+        validate_key(key.key, suspicious, reference_value, cleaned, errors)
+    elif key in suspicious:
+        validate_value(key, suspicious[key], reference_value, cleaned, errors)
+    else:
+        #missing key error!
+        print("missing key error")
 
 def validate_value(key, value, reference_value, cleaned, errors):
     is_valid = True
@@ -47,38 +85,7 @@ def validate_value(key, value, reference_value, cleaned, errors):
 
 def validate(schema, suspicious, cleaned, errors):
     for key, reference_value in schema.items():
-        if isinstance(key, Optional):
-            key = key.key
-            if key not in suspicious:
-                continue
-        elif key == Or:
-            validated = False
-            for orkey, orvalue in reference_value.items():
-                if not orkey in suspicious:
-                    continue
-                validated = validate_value(orkey, suspicious[orkey],
-                               orvalue, cleaned, errors)
-            if not validated:
-                errors.section_errors.append(
-                    "Missing any of {}".format(reference_value.keys()))
-            continue
-        elif key == XOr:
-            validated = 0
-            for orkey, orvalue in reference_value.items():
-                if not orkey in suspicious:
-                    continue
-                if validate_value(orkey, suspicious[orkey],
-                                  orvalue, cleaned, errors):
-                    validated += 1
-            if validated == 0:
-                errors.section_errors.append(
-                    "Missing one of {}".format(reference_value.keys()))
-            elif validated > 1:
-                errors.section_errors.append(
-                    "Only one of {} permitted".format(reference_value.keys()))
-            continue
-
-        validate_value(key, suspicious[key], reference_value, cleaned, errors)
+        validate_key(key, suspicious, reference_value, cleaned, errors)
 
 class Form:
     def __init__(self, schema):
